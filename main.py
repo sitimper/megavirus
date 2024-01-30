67d5d37c21159cab8869b9d61b785954f774ffbf
sitimper <sitimper@gmail.com>
2024-01-30 11:39:22 +0100

    virus v0.1
    full functional virus for infis servers

---
+++ main.py
@@ -0,0 +1,12 @@
+# - - - - - - - - - - - - - - - -
+#
+#   MEGAVIRUS FOR INFIS SERVERS
+#
+# - - - - - - - - - - - - - - - -
+
+from time import sleep
+
+while True:
+    sleep(0.1)
+    print("INFIS SERVERS WERE HACKED")
+    
